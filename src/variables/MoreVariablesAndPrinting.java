7edf439adc73524abdd3604ae67856154fe06bd7
Alex Vakulenko <pijamkyn@gmail.com>
2016-02-23 20:44:30 -0500

    modify print method

--- src/variables/MoreVariablesAndPrinting.java
+++ src/variables/MoreVariablesAndPrinting.java
@@ -29,7 +29,8 @@ public class MoreVariablesAndPrinting {
     System.out.println("Actually, that's not too heavy.");
     System.out.println("He's got " + eyes + " eyes and " + hair + " hair.");
     System.out.println("His teeth are usually " + teeth + " depending on the coffee.");
-
+    System.out.println();
+    System.out.println();
     // This line is tricky; try to get it exactly right.
     System.out.println("If I add " + age + ", " + height + ", and " + weight + " I get " + (age + height + weight) + ".");
   }
