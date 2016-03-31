c6045d320b02f683776d11db9e13e1145add8f71
Alex Vakulenko <pijamkyn@gmail.com>
2016-03-31 15:58:39 -0400

    upd2

--- src/loops/Nim.java
+++ src/loops/Nim.java
@@ -4,26 +4,22 @@ import java.util.Scanner;
 
 public class Nim {
 
-  static int A;
-  static int B;
-  static int C;
-
   public static void main(String[] args) {
     // TODO Auto-generated method stub
 
     String player1;
     String player2;
+    String nextPlayer;
 
-    int firstPlayerA = 3;
-    int firstPlayerB = 4;
-    int firstPlayerC = 5;
+    int a = 3;
+    int b = 4;
+    int c = 5;
 
-    int secondPlayerA = 3;
-    int secondPlayerB = 4;
-    int secondPlayerC = 5;
     int toRemove;
-
+    int result;
     char target;
+    boolean isEmpty = false;
+
     char pileA = 'A';
     char pileB = 'B';
     char pileC = 'C';
@@ -31,27 +27,116 @@ public class Nim {
     Scanner keyboard = null;
     keyboard = new Scanner(System.in);
 
-    // enter players quantity
+    // enter players
+
     System.out.print("Player 1, enter your name: ");
     player1 = keyboard.next();
 
     System.out.print("Player 2, enter your name: ");
     player2 = keyboard.next();
 
-    System.out.println("A: " + firstPlayerA + "B: " + firstPlayerB + "C: " + firstPlayerC);
+    // print controls
+    System.out.println("A: " + a + "  B: " + b + "  C: " + c);
 
-    System.out.print(player1 + ", choose a pile: ");
-    target = keyboard.next().charAt(0);
+    nextPlayer = player1;
+    do {
 
-    if (target != 'A' | target != 'B' | target != 'C') {
-      do {
-        System.out.print(player1 + ", choose a pile. You con choose A or B or C ");
+      if (nextPlayer.equals(player1)) {
+
+        System.out.print(player1 + ", choose a pile: ");
         target = keyboard.next().charAt(0);
-      } while (target == 'A' | target == 'B' | target == 'C');
-    }
 
-    System.out.print("How many to remove from pile " + target);
-    toRemove = keyboard.nextInt();
+        if (target == pileA) {
+          if (a <= 0) {
+            isEmpty = true;
+          } else {
+            isEmpty = false;
+          }
 
+        } else if (target == pileB) {
+          if (b <= 0) {
+            isEmpty = true;
+          } else {
+            isEmpty = false;
+          }
+        } else if (target == pileC) {
+          if (c <= 0) {
+            isEmpty = true;
+          } else {
+            isEmpty = false;
+          }
+        }
+
+        if (isEmpty == true) {
+
+          System.out.println("Nice try, " + player1 + ". That pile is empty. Choose again: ");
+
+        } else {
+
+          System.out.print("How many to remove from pile " + target);
+          toRemove = keyboard.nextInt();
+
+          if (target == pileA) {
+            a = a - toRemove;
+          } else if (target == pileB) {
+            b = b - toRemove;
+          } else if (target == pileC) {
+            c = c - toRemove;
+          }
+          System.out.println("A: " + a + "  B: " + b + "  C: " + c);
+
+          nextPlayer = player2;
+        }
+
+      } else if (nextPlayer.equals(player2)) {
+
+        System.out.print(player2 + ", choose a pile: ");
+        target = keyboard.next().charAt(0);
+
+        if (target == pileA) {
+          if (a <= 0) {
+            isEmpty = true;
+          } else {
+            isEmpty = false;
+          }
+
+        } else if (target == pileB) {
+          if (b <= 0) {
+            isEmpty = true;
+          } else {
+            isEmpty = false;
+          }
+        } else if (target == pileC) {
+          if (c <= 0) {
+            isEmpty = true;
+          } else {
+            isEmpty = false;
+          }
+        }
+
+        if (isEmpty == true) {
+          System.out.println("Nice try, " + player2 + ". That pile is empty. Choose again: ");
+
+        } else {
+
+          System.out.print("How many to remove from pile " + target);
+          toRemove = keyboard.nextInt();
+
+          if (target == pileA) {
+            a = a - toRemove;
+          } else if (target == pileB) {
+            b = b - toRemove;
+          } else if (target == pileC) {
+            c = c - toRemove;
+          }
+          System.out.println("  A: " + a + "  B: " + b + "  C: " + c);
+          nextPlayer = player1;
+        }
+      }
+
+    } while (a > 0 | b > 0 | c > 0);
+
+    System.out.println(nextPlayer + ", there are no counters left, so you WIN!");
   }
+
 }
---
+++ src/loops/NimVersion3.java
@@ -0,0 +1,10 @@
+package loops;
+
+public class NimVersion3 {
+
+  public static void main(String[] args) {
+    // TODO Auto-generated method stub
+
+  }
+
+}
